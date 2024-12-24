17c419fea504815f587495f20d5ac28b19f9e46e
Clynova <clynova.dev@gmail.com>
2024-12-24 00:17:49 -0300

    Se finalizaron las funciones v2

--- fase12/js/app4.js
+++ fase12/js/app4.js
@@ -3,3 +3,20 @@ const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
 
 const meses2 = [...meses, 'Agosto']
 console.log(meses2)
+
+
+const carrito = [
+    { nombre: 'Monitor 1 pulgadas', precio: 100 },
+    { nombre: 'Television', precio: 200 },
+    { nombre: 'Tablet', precio: 300 },
+    { nombre: 'Audifonos', precio: 400 },
+    { nombre: 'Teclado', precio: 500 },
+    { nombre: 'Celular', precio: 600 },
+];
+
+
+const producto = { nombre: 'Tetera', precio: 500 }
+
+const carrito2 = [producto, ...carrito]
+
+console.log(carrito2)
